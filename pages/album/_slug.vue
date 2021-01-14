baa66b226fbc7a4d2ebd8239bfd18e7232708f04
Jon Muller <jon.muller@gmail.com>
2021-01-14 11:13:55 +1100

    Slug fix

--- pages/album/_slug.vue
+++ pages/album/_slug.vue
@@ -2,7 +2,7 @@
 	<div class="content">
 		<h1>{{ album.artist }} - {{ album.title }}</h1>
 		<ul>
-			<li v-for="artist in album.artists" :key="artist" v-text="artists.filter(el => (el.id === artist))[0].name" />
+			<li v-for="artist in album.artists" :key="artist" v-text="artists.filter(el => el.id === artist)[0].name" />
 		</ul>
 	</div>
 </template>
@@ -17,8 +17,13 @@ export default {
 			return this.$store.state.wuData.albums;
 		},
 		album() {
-			return this.albums.find(el => el.album === this.slug);
+			return this.albums.find(el => el.title.toLowerCase() === this.slug);
 		}
+	},
+	data() {
+		return {
+			slug: this.$route.params.slug
+		};
 	}
 };
 </script>
