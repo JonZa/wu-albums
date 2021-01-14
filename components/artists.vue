f923d6b82c0b3ed24023aa9c6122bd1e6626f089
Jon Muller <jon.muller@gmail.com>
2021-01-15 09:05:43 +1100

    Artist watch function

--- components/artists.vue
+++ components/artists.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="artists">
-		<form class="artists__filter" @submit="submitForm" ref="form">
+		<form class="artists__filter" ref="form">
 			<label for="artists__input" class="artists__label">Filter by artist:</label>
 			<input id="artists__input" type="text" class="artists__input" v-model="artistName" ref="filter" @focus="focus(true)" @blur="focus(false)" />
 			<div class="artists__dropdown" v-if="artistName.length > 1 && filterFocus">
@@ -20,6 +20,19 @@ export default {
 			filterFocus: false
 		};
 	},
+	watch: {
+		artistName: function(newValue, oldValue) {
+			const selectedArtist = this.wuData.artists.filter(function(artist) {
+				return artist.name.toLowerCase() === newValue.toLowerCase();
+			});
+			console.log(selectedArtist);
+			if (selectedArtist.length === 1) {
+				this.$store.commit('updateSelectedArtist', selectedArtist[0].id);
+			} else {
+				this.$store.commit('updateSelectedArtist', null);
+			}
+		}
+	},
 	components: {
 		Album
 	},
@@ -34,27 +47,9 @@ export default {
 	},
 	methods: {
 		focus(value) {
-			console.log('focus');
-			console.log(value);
 			return (this.filterFocus = value);
 		},
-		submitForm(event) {
-			console.log('submitForm');
-			console.log(event);
-			event.preventDefault();
-			const artistName = this.artistName;
-			const selectedArtist = this.wuData.artists.filter(function(artist) {
-				return artist.name.toLowerCase() === artistName.toLowerCase();
-			});
-			if (selectedArtist.length) {
-				this.$store.commit('updateSelectedArtist', selectedArtist[0].id);
-			} else {
-				this.$store.commit('updateSelectedArtist', null);
-			}
-		},
 		updateSelectedArtist(artist) {
-			console.log('updateSelectedArtist');
-			console.log(artist);
 			this.artistName = artist.name;
 			this.$store.commit('updateSelectedArtist', artist.id);
 			this.focus(false);
